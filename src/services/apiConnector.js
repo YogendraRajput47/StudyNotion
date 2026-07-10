528b4061be9ae66a6ada40f013bdc814a0d9cf6e
YogendraRajput47 <yogendra.leetcode123@gmail.com>
2026-07-10 10:47:33 +0530

    Change API URL in apiConnector
    Updated the API URL to point to the new endpoint.

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -5,7 +5,7 @@ export const axiosInstance = axios.create({});
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
     method: `${method}`,
-    url: `${url}`,
+    url: `${https://studynotion-ofuz.onrender.com/api/v1}`,
     data: bodyData ? bodyData : null,
     headers: headers ? headers : null,
     params: params ? params : null,
